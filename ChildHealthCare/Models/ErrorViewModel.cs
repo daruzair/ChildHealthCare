d9046c68c0a83d3cde39044003bc3bc7e7c77267
uzair <daruzair420@gmail.com>
2022-11-03 12:05:27 +0530

    Add project files.

---
+++ ChildHealthCare/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChildHealthCare.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
